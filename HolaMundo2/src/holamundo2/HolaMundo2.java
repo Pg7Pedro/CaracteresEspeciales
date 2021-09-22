fe260c1b56f8c4851a488df9a102a6fe18de64a2
Pg7Pedro <Pg91999@hotmail.com>
2021-09-22 12:19:51 +0200

    Initial commit

---
+++ HolaMundo2/src/holamundo2/HolaMundo2.java
@@ -0,0 +1,16 @@
+package holamundo2;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class HolaMundo2 {
+    
+    public static void main(String[] args) {
+        
+        String miVar = "Hola profesor";
+        System.out.println("miVar = " + miVar);
+        System.out.println();
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CaracteresEspeciales
+ CaracteresEspecialesJAVA
